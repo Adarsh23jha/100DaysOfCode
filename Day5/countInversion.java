3507a101ae14727188bf8c7f2d1c76616dcd286d
Adarsh Jha <adarsh05jha@gmail.com>
2021-07-20 21:39:51 +0530

    Day 5 Completed

---
+++ Day5/countInversion.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class countInversion {
+    static int Inversion(int a[]) {
+        int counter = 0;
+        for (int j = 0; j < a.length - 1; j++) {
+            for (int i = j + 1; i < a.length; i++) {
+                if (a[j]  a[i]) {
+                    counter++;
+
+                }
+            }
+        }
+        System.out.println(counter);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+
+        System.out.println("Enter the array size : ");
+        n = in.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.print("Enter The array element : ");
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        Inversion(array);
+        // System.out.println(Inversion(array));
+
+        in.close();
+    }
+}
---
+++ Day5/longestArithmeticSubarray.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class longestArithmeticSubarray {
+    static int calculate(int a[], int k) {
+
+        int temp[] = new int[k];
+        int n = k - 1, counter = 0, max = Integer.MIN_VALUE;
+        for (int i = n; i > 0; i--) {
+            int diff = a[i - 1] - a[i];
+            temp[i] = diff;
+        }
+        for (int i = n; i > 0; i--) {
+
+            if (temp[i - 1] == temp[i]) {
+
+                counter = counter + 1;
+
+                max = Math.max(max, counter);
+                break;
+
+            } else {
+                counter = 0;
+            }
+
+        }
+
+        int v = max + 2;
+
+        return v;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+
+        System.out.println("Enter the array size : ");
+        n = in.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.print("Enter The array element : ");
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        // calculate(array, n);
+        System.out.println(calculate(array, n));
+
+        in.close();
+
+    }
+
+}
